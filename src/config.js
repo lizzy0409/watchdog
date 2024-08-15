1d4d2104aab54f1a0ade425e96a7698fe9d43a8f
helios <lizzy040925@gmail.com>
2024-08-15 17:26:03 +0900

    fix:walletconnect implementation

--- src/config.js
+++ src/config.js
@@ -1,8 +1,27 @@
 import { http, createConfig } from 'wagmi'
 import { base, bsc, mainnet, optimism } from 'wagmi/chains'
 import { coinbaseWallet, injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+// import { Core } from '@walletconnect/core'
+// import { Web3Wallet } from '@walletconnect/web3wallet'
 
-const projectId = '<WALLETCONNECT_PROJECT_ID>'
+// const core = new Core({
+//   projectId: 'fa3d84f47653d4f84cb0268e691ef692'
+// })
+
+// const metadata = {
+//   name: 'watchdogswap',
+//   description: 'AppKit Example',
+//   url: 'https://web3modal.com', // origin must match your domain & subdomain
+//   icons: ['https://avatars.githubusercontent.com/u/37784886']
+// }
+
+// const web3wallet = await Web3Wallet.init({
+//   core, // <- pass the shared 'core' instance
+//   metadata
+// })
+
+
+const projectId = 'fa3d84f47653d4f84cb0268e691ef692'
 
 export const config = createConfig({
   chains: [mainnet, base, bsc],
